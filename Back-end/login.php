1aca7f2f524bda28e67000a3e381002783295161
Diogo Pavani <pavanidiogo1@gmail.com>
2024-09-25 19:07:00 -0300

    :bug: corrigido login sem verificar o email

--- Back-end/login.php
+++ Back-end/login.php
@@ -2,15 +2,13 @@
 
 session_start();
 
-
-header("Access-Control-Allow-Origin: http://localhost:3000"); //colocar seu ip 
+header("Access-Control-Allow-Origin: http://localhost:3000"); // permite requisições do front, lembra de verificar se o ip é o mesmo
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json'); 
 
 require 'database/datab.php';
-
 
 $data = json_decode(file_get_contents('php://input'), true);
 $email = $data['email'] ?? '';  
@@ -22,36 +20,33 @@ $response = [
     'user' => null
 ];
 
-
 if (!empty($email) && !empty($password)) {
 
-    $stmt = $pdo->prepare("SELECT id, password FROM usuarios WHERE email = ?");
+    $stmt = $pdo->prepare("SELECT id, password, ativado FROM usuarios WHERE email = ?");
     $stmt->execute([$email]);
     $user = $stmt->fetch();
 
-    
     if ($user && password_verify($password, $user['password'])) {
-        
-        $_SESSION['user_id'] = $user['id'];
-        $_SESSION['user_email'] = $email;  
-        $_SESSION['inicioSessao'] = time();  
+        if ($user['ativado']) {
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['user_email'] = $email;  
+            $_SESSION['inicioSessao'] = time();  
 
-        
-        $response['logado'] = true;
-        $response['message'] = 'Login efetuado com sucesso.';
-        $response['user'] = [
-            'id' => $user['id'],
-            'email' => $email
-        ];
+            $response['logado'] = true;
+            $response['message'] = 'Login efetuado com sucesso.';
+            $response['user'] = [
+                'id' => $user['id'],
+                'email' => $email
+            ];
+        } else {
+            $response['message'] = 'Conta não ativada. Por favor, verifique seu email para ativar a conta.';
+        }
     } else {
-        
         $response['message'] = 'Email ou senha incorretos.';
     }
 } else {
-    
     $response['message'] = 'Falta email ou senha.';
 }
-
 
 echo json_encode($response);
 ?>
