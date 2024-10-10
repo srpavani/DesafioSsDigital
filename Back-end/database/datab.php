f2e8e8548975105c5e76c9351bcdeb964ab59c9c
Diogo Pavani <pavanidiogo1@gmail.com>
2024-10-10 10:35:39 -0300

    add port no .env

--- Back-end/database/datab.php
+++ Back-end/database/datab.php
@@ -2,13 +2,13 @@
 
 require 'env.php'; 
 
-loadEnv(__DIR__ . '/.env'); //funcao para conseguir usar .env
+loadEnv(__DIR__ . '/.env');
 
 $host = getenv('DB_HOST');
 $db = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASSWORD');
-$port = 5439;
+$port =getenv('DB_PORT');
 
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
@@ -21,4 +21,5 @@ try {
 } catch (\PDOException $e) {
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
+
 ?>
